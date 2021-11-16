e6c3c741ca6814732bfbd3efad4b47125bd835d4
Giovanni Calleja <giocallejam@gmail.com>
2021-11-16 03:15:05 -0600

    se agrego carrusel a productos

--- src/assets/js/product-img.js
+++ src/assets/js/product-img.js
@@ -2,3 +2,11 @@ function myFuction(smallImg){
     var fullImg = document.getElementById("imagebox");
     fullImg.src = smallImg.src;
 }
+
+/* $(document).ready(function(){
+    $('.carousel').slick({
+    slidesToShow: 3,
+    dots:true,
+    centerMode: true,
+    });
+  }); */
